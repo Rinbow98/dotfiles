24d77fb2ac00e1c842f6bd50e68fea2fb809b450
Rinbow <ray123546@gmail.com>
2026-04-09 01:04:16 +0800

    fix command missing

--- .config/zsh/tool.zsh
+++ .config/zsh/tool.zsh
@@ -2,14 +2,14 @@
 # Tool
 # ================================
 
+# linuxbrew
+# run_cmd /home/linuxbrew/.linuxbrew/bin/brew 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"'
+
 # Prompt
 run_cmd starship 'eval "$(starship init zsh)"'
 
 # Shell Env
 run_cmd zoxide 'eval "$(zoxide init zsh)"'
-
-# linuxbrew
-# run_cmd /home/linuxbrew/.linuxbrew/bin/brew 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"'
 
 # Node version manager
 # run_cmd fnm 'eval "$(fnm env --use-on-cd)"'
